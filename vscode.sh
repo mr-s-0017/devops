a36f84b24e9c080aa0559cda52322a245117198c
sahil <sahilkapoor0017@gmail.com>
2024-02-02 12:44:11 +0530

    added new processes

--- vscode.sh
+++ vscode.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update
 sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
